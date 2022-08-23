ba94210642435430ba492e0084cc4f7effd1a8ca
abar ahmed <abrarahmed2185@gmail.com>
2022-08-23 16:43:36 +0500

    Setup MongoDB and Hanlder

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const connectDB = require("./config/db");
+
 // Setup Port
 const PORT = process.env.PORT || 5000;
 
 // Load Env File
 dotenv.config({ path: "./config/config.env" });
 
+// Connect Database
+connectDB();
+
 // Import Bootcamp Router
 
 const bootcamps = require("./routes/bootcamps");
-
 
 const app = express();
 
 app.use("/api/v1/bootcamps", bootcamps);
 
+const server = app.listen(PORT, console.log(`Server listening on ${PORT}`));
 
+// Handle unhandled promise Rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
 
-app.listen(PORT, console.log(`Server listening on ${PORT}`));
+  // Close Server
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
