b5ff4e1fc4fb75799af095d9d416bd764e203016
abar ahmed <abrarahmed2185@gmail.com>
2022-08-30 00:44:33 +0500

    Handle Mongoose Error Handler

--- middleware/error.js
+++ middleware/error.js
@@ -1,14 +1,28 @@
 const ErrorResponse = require("../utils/errorResponse");
 
 const erroHandler = function (err, req, res, next) {
-   //    console.log(err.stack.red);
+   console.log(err);
 
    let error = { ...err };
 
    error.message = err.message;
 
+   //    Mongoose Invalid Id Error
+
    if (err.name == "CastError") {
       const message = `Not Found Id: ${err.value}`;
+      error = new ErrorResponse(message, 404);
+   }
+
+   //    Mongoose Duplicate Data Error
+   if (err.code == 11000) {
+      const message = "Duplicate Data Found...";
+      error = new ErrorResponse(message, 404);
+   }
+
+   //    Mongoose Validation Error
+   if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
       error = new ErrorResponse(message, 404);
    }
 
